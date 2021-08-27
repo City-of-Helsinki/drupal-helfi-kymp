696cd7879db2b8bb7381055f8ea104ea2107efec
tuutti <tuutti@iki.fi>
2021-08-27 10:55:34 +0300

    UHF-2060: Blob storage settings, env specific "site prefixes"

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-maps-and-transport',
+  'fi' => 'dev-kartat-ja-liikenne',
+  'sv' => 'dev-kartor-och-trafik',
+  'ru' => 'dev-maps',
+];
---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-maps-and-transport',
+  'fi' => 'test-kartat-ja-liikenne',
+  'sv' => 'test-kartor-och-trafik',
+  'ru' => 'test-maps',
+];
