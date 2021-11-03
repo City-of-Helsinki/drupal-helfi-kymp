97d4a1b2d192c29b0be44c3373f916ad38e4e277
tuutti <tuutti@iki.fi>
2021-11-03 11:11:42 +0200

    UHF-3133: Run cr before cim

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -23,8 +23,8 @@ $(PROJECT_DIR)/vendor:
 PHONY += install-drupal
 install-drupal:
 	$(call docker_run_ci,drush si --existing-config -y)
-	$(call docker_run_ci,drush cim -y)
 	$(call docker_run_ci,drush cr)
+	$(call docker_run_ci,drush cim -y)
 
 PHONY += start-project
 start-project: $(STONEHENGE_PATH)/.git
