b2ff2c18b12b68b2c2c37bc3ebf8e1ffb39df8f9
tuutti <tuutti@iki.fi>
2021-11-03 09:17:11 +0200

    UHF-3133: Setup proxy

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -1,6 +1,14 @@
 STONEHENGE_PATH ?= ${HOME}/stonehenge
 PROJECT_DIR ?= ${GITHUB_WORKSPACE}
 DOCKER_COMPOSE_FILES = -f docker-compose.ci.yml -f docker-compose.yml
+DOCKER_PROXY_PATH ?= ${HOME}/proxy
+
+$(DOCKER_PROXY_PATH)/.git:
+	git clone https://github.com/City-of-Helsinki/drupal-helfi-local-proxy.git $(DOCKER_PROXY_PATH)
+
+PHONY += start-proxy
+start-proxy: $(DOCKER_PROXY_PATH)
+	./start.sh ${PROXY_PROJECT_NAME} &
 
 $(STONEHENGE_PATH)/.git:
 	git clone -b 3.x https://github.com/druidfi/stonehenge.git $(STONEHENGE_PATH)
@@ -17,7 +25,7 @@ install-drupal:
 	$(call docker_run_ci, drush si --existing-config -y)
 
 PHONY += start-project
-start-project:
+start-project: $(STONEHENGE_PATH)/.git
 	docker compose $(DOCKER_COMPOSE_FILES) up -d
 
 PHONY += set-permissions
@@ -29,4 +37,4 @@ define docker_run_ci
 endef
 
 PHONY += setup-ci
-setup-ci: set-permissions $(STONEHENGE_PATH)/.git start-stonehenge start-project $(PROJECT_DIR)/vendor install-drupal
+setup-ci: set-permissions start-stonehenge start-project $(PROJECT_DIR)/vendor install-drupal start-proxy
