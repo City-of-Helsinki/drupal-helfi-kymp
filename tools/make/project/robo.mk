b69ac42ee6ec6574de70190905c7888a3cd9a2d2
tuutti <tuutti@iki.fi>
2021-11-03 08:14:53 +0200

    UHF-3133: Make commands to setup ci

---
+++ tools/make/project/robo.mk
@@ -0,0 +1,31 @@
+STONEHENGE_PATH ?= ${HOME}/stonehenge
+PROJECT_DIR ?= ${GITHUB_WORKSPACE}
+
+$(STONEHENGE_PATH)/.git:
+	git clone -b 3.x https://github.com/druidfi/stonehenge.git $(STONEHENGE_PATH)
+
+PHONY += start-stonehenge
+start-stonehenge:
+	cd $(STONEHENGE_PATH) && make up
+
+$(PROJECT_DIR)/vendor:
+	$(call docker_run_ci, 'composer install')
+
+PHONY += install-drupal
+install-drupal:
+	$(call docker_run_ci, 'drush si --existing-config -y')
+
+PHONY += start-project
+start-project:
+	$(call docker_run_ci, 'up -d')
+
+PHONY += set-permissions
+set-permissions:
+	chmod 777 -R $(PROJECT_DIR)
+
+define docker_run_ci
+	docker compose -f docker-compose.ci.yml -f docker-compose.yml $(1)
+endef
+
+PHONY += setup-ci
+setup-ci: set-permissions $(STONEHENGE_PATH)/.git start-stonehenge $(PROJECT_DIR)/vendor start-project install-drupal
