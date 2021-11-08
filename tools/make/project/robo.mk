1edb5937dd2630363b279b5e3b5ae8d0446aacf9
tuutti <tuutti@iki.fi>
2021-11-08 07:39:32 +0200

    UHF-3133: Updated tools, refactored makefile to work on local env too

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -2,11 +2,19 @@ STONEHENGE_PATH ?= ${HOME}/stonehenge
 PROJECT_DIR ?= ${GITHUB_WORKSPACE}
 DOCKER_COMPOSE_FILES = -f docker-compose.ci.yml -f docker-compose.yml
 
+ifeq ($(CI),true)
+	SETUP_ROBO_TARGETS := set-permissions install-stonehenge start-stonehenge start-project $(PROJECT_DIR)/vendor install-drupal
+else
+	SETUP_ROBO_TARGETS := start-project $(PROJECT_DIR)/vendor install-drupal
+endif
+
+install-stonehenge: $(STONEHENGE_PATH)/.git
+
 $(STONEHENGE_PATH)/.git:
 	git clone -b 3.x https://github.com/druidfi/stonehenge.git $(STONEHENGE_PATH)
 
 PHONY += start-stonehenge
-start-stonehenge: $(STONEHENGE_PATH)/.git
+start-stonehenge:
 	cd $(STONEHENGE_PATH) && make up
 
 $(PROJECT_DIR)/vendor:
@@ -20,7 +28,7 @@ install-drupal:
 	$(call docker_run_ci,app,drush cim -y)
 
 PHONY += start-project
-start-project: $(STONEHENGE_PATH)/.git
+start-project:
 	docker compose $(DOCKER_COMPOSE_FILES) up -d
 
 PHONY += set-permissions
@@ -31,9 +39,9 @@ define docker_run_ci
 	docker compose $(DOCKER_COMPOSE_FILES) exec $(1) bash -c "$(2)"
 endef
 
-PHONY += setup-ci
-setup-ci: set-permissions start-stonehenge start-project $(PROJECT_DIR)/vendor install-drupal
+PHONY += setup-robo
+setup-robo: $(SETUP_ROBO_TARGETS)
 
 PHONY += run-tests
 run-tests:
-	$(call docker_run_ci,robo,curl curl http://helfi-${PROJECT_NAME}-varnish:6081)
+	$(call docker_run_ci,robo,curl curl http://${COMPOSE_PROJECT_NAME}-varnish:6081)
