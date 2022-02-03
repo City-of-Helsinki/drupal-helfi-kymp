b0aa8720e01865dbb848b668e08e96980a03344a
tuutti <tuutti@iki.fi>
2022-02-03 16:46:29 +0200

    UHF-3505: Re-enable redis

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -13,13 +13,20 @@ if (
   ($redis_host = getenv('REDIS_HOST')) &&
   file_exists('modules/contrib/redis/example.services.yml')
 ) {
-  // $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
-  // $redis_port = getenv('REDIS_PORT') ?: 6379;
+  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+  $redis_port = getenv('REDIS_PORT') ?: 6379;
 
-  // $settings['redis.connection']['interface'] = 'Predis';
-  // $settings['redis.connection']['host'] = $redis_host;
-  // $settings['redis.connection']['port'] = getenv('REDIS_PORT') ?: 6379;
-  // $settings['cache']['default'] = 'cache.backend.redis';
-  // $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
-  // $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+  if ($redis_prefix = getenv('REDIS_PREFIX')) {
+    $settings['cache_prefix']['default'] = $redis_prefix;
+  }
+
+  if ($redis_password = getenv('REDIS_PASSWORD')) {
+    $settings['redis.connection']['password'] = $redis_password;
+  }
+  $settings['redis.connection']['interface'] = 'Predis';
+  $settings['redis.connection']['host'] = $redis_host;
+  $settings['redis.connection']['port'] = $redis_port;
+  $settings['cache']['default'] = 'cache.backend.redis';
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
 }
