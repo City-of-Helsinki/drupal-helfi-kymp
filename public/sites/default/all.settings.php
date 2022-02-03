cd9eeeec2fb250c0cb4fdc1b941857b4d2f5d4eb
tuutti <tuutti@iki.fi>
2022-02-03 15:13:04 +0200

    UHF-3505: Disable redis for now

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -13,13 +13,13 @@ if (
   ($redis_host = getenv('REDIS_HOST')) &&
   file_exists('modules/contrib/redis/example.services.yml')
 ) {
-  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
-  $redis_port = getenv('REDIS_PORT') ?: 6379;
+  // $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+  // $redis_port = getenv('REDIS_PORT') ?: 6379;
 
-  $settings['redis.connection']['interface'] = 'Predis';
-  $settings['redis.connection']['host'] = $redis_host;
-  $settings['redis.connection']['port'] = getenv('REDIS_PORT') ?: 6379;
-  $settings['cache']['default'] = 'cache.backend.redis';
-  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
-  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+  // $settings['redis.connection']['interface'] = 'Predis';
+  // $settings['redis.connection']['host'] = $redis_host;
+  // $settings['redis.connection']['port'] = getenv('REDIS_PORT') ?: 6379;
+  // $settings['cache']['default'] = 'cache.backend.redis';
+  // $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+  // $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
 }
