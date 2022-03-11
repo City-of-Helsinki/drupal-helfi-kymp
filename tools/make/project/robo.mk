bce32ac855ad0f72e2f7d5d8c13d375e3034a6dd
tuutti <tuutti@iki.fi>
2022-03-11 09:42:51 +0200

    Added robo argument

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -87,4 +87,4 @@ setup-robo: $(SETUP_ROBO_TARGETS)
 
 PHONY += run-robo-tests
 run-robo-tests:
-	$(call docker_run_ci,robo,cd /app/helfi-test-automation-python && pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 $(ROBOT_EXTRA_ARGS) -A environments/ci.args -v PREFIX:$(SITE_PREFIX) -v BASE_URL:varnish-$(DRUPAL_HOSTNAME) -v PICCOMPARE:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -d robotframework-reports  .)
+	$(call docker_run_ci,robo,cd /app/helfi-test-automation-python && pabot --testlevelsplit --ordering ./environments/helfi_pabot_order_ci --processes 9 $(ROBOT_EXTRA_ARGS) -v useoriginalname:False -A environments/ci.args -v PREFIX:$(SITE_PREFIX) -v BASE_URL:varnish-$(DRUPAL_HOSTNAME) -v PICCOMPARE:False -v images_dir:robotframework-resources/screenshots/headlesschrome -v actual_dir:robotframework-reports -d robotframework-reports  .)
