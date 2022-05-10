368acd9e11e91f76aa8c684d7189fdf891b8de07
tuutti <tuutti@iki.fi>
2022-05-10 08:32:26 +0300

    Tunnistamo return URLs

--- public/sites/default/development.settings.php
+++ public/sites/default/development.settings.php
@@ -1,5 +1,6 @@
 <?php
 
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/dev-kaupunkiymparisto-ja-liikenne/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'dev-liikenne-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'dev-urban-environment-and-traffic',
--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -1,5 +1,6 @@
 <?php
 
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/staging-kaupunkiymparisto-ja-liikenne/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'staging-liikenne-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'staging-urban-environment-and-traffic',
