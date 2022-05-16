39fc1d9ade14967726b59abd13203621fc3e28ec
Lauri Kolehmainen <lauri.kolehmainen@siili.com>
2022-05-16 22:14:14 +0300

    UHF-5523: javascript fixes after template changes

--- public/modules/custom/helfi_kymp_plans/assets/js/list-of-plans.js
+++ public/modules/custom/helfi_kymp_plans/assets/js/list-of-plans.js
@@ -4,29 +4,31 @@
   Drupal.behaviors.list_of_plans = {
     attach: function attach(context, settings) {
       // Show only 8 items at first.
-      let initialPlanCount = $(".list-of-plans__plan").length;
+      let initialPlanCount = $('.list-of-plans__plan').length;
 
       let numberOfItems = 8;
       let showMoreItems = 8;
 
       if (initialPlanCount > numberOfItems) {
-        $(".list-of-plans__plan").hide();
-        $(".list-of-plans__plan:lt(" + numberOfItems + ")").show();
+        $('.list-of-plans__plan').hide();
+        $('.list-of-plans__plan:lt(' + numberOfItems + ')').show();
       }
 
       // Show more items when clicking the button.
-      $(".pager-button").click(function(e) {
+      $('.pager-button').click(function(e) {
         e.preventDefault();
 
         numberOfItems = numberOfItems + showMoreItems;
-        $(".list-of-plans__plan:lt(" + numberOfItems + ")").show();
+        $('.list-of-plans__plan:lt(' + numberOfItems + ')').show();
 
         // Scroll to the bottom of the list.
-        //TODO
+        $('html').animate({
+          scrollTop: $('.list-of-plans__list')[0].scrollHeight
+        }, 1000);
 
         // Hide the button when all items are visible.
         if (numberOfItems >= initialPlanCount) {
-          $(".pager-button").hide();
+          $('.pager-button').hide();
         }
       });
     }
