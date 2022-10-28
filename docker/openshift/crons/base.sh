9d851ad5fd14dcce691f6d59d766abfdcd390053
tuutti <tuutti@iki.fi>
2022-10-28 07:43:29 +0300

    UHF-7265: Removed -q flag from cron

--- docker/openshift/crons/base.sh
+++ docker/openshift/crons/base.sh
@@ -24,7 +24,7 @@ exec "/crons/content-scheduler.sh" &
 while true
 do
   echo "Running cron: $(date +'%Y-%m-%dT%H:%M:%S%:z')\n"
-  drush cron -q
+  drush cron
   # Sleep for 10 minutes.
   sleep 600
 done
