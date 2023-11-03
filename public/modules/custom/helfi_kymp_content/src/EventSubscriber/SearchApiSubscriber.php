014e8ef51c4f864aeb7a5b67a8dd9d9f358e3019
rpnykanen <risto.nykanen@druid.fi>
2023-11-03 13:53:51 +0200

    UHF-9201: inject logger

--- public/modules/custom/helfi_kymp_content/src/EventSubscriber/SearchApiSubscriber.php
+++ public/modules/custom/helfi_kymp_content/src/EventSubscriber/SearchApiSubscriber.php
@@ -4,12 +4,22 @@ namespace Drupal\helfi_kymp_content\EventSubscriber;
 
 use Drupal\search_api\Event\ReindexScheduledEvent;
 use Drupal\search_api\Event\SearchApiEvents;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Subscribe to search api events.
  */
 class SearchApiSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The constructor.
+   *
+   * @param LoggerInterface $logger
+   *   The logger.
+   */
+  public function __construct(private LoggerInterface $logger) {
+  }
 
   /**
    * {@inheritDoc}
@@ -39,9 +49,7 @@ class SearchApiSubscriber implements EventSubscriberInterface {
         }
       }
       catch (\Exception $e) {
-        \Drupal::logger('helfi_kymp_content')->error(
-          'unable to fetch data while running reindex event'
-        );
+        $this->logger('unable to fetch data while running reindex event');
       }
 
       $index->trackItemsInserted($source->getPluginId(), array_keys($data));
