aab77a4d70d9bfd40a343b8a96945716ecf4557b
rpnykanen <risto.nykanen@druid.fi>
2023-11-03 12:34:29 +0200

    UHF-9201: get index from event

--- public/modules/custom/helfi_kymp_content/src/EventSubscriber/SearchApiSubscriber.php
+++ public/modules/custom/helfi_kymp_content/src/EventSubscriber/SearchApiSubscriber.php
@@ -19,47 +19,35 @@ class SearchApiSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       SearchApiEvents::REINDEX_SCHEDULED => ['reindex'],
-      SearchApiEvents::GATHERING_PROCESSORS => ['disableProcessor'],
     ];
-  }
-
-  /**
-   * Disable incompatible processor.
-   *
-   * @param GatheringPluginInfoEvent $event
-   *   The event.
-   */
-  public function disableProcessor(GatheringPluginInfoEvent $event) {
-    $processors = $event->getDefinitions();
-    if (isset($processors['media_reference_to_object'])) {
-      unset($processors['media_reference_to_object']);
-    }
   }
 
   /**
    * Reindex event.
    *
-   * @param ReindexScheduledEvent $event
+   * @param Drupal\search_api\Event\ReindexScheduledEvent $event
    *   The event.
    *
    * @throws \Drupal\search_api\SearchApiException
    */
   public function reindex(ReindexScheduledEvent $event): void {
-    $index = Index::load('street_data');
-    $source = $index->getDatasource('external_source');
-    try {
-      $data = $source->loadMultiple([]);
-      if (!$data) {
-        return;
+    $index = $event->getIndex();
+    if ($index->id() == 'street_data') {
+      $source = $index->getDatasource('external_source');
+      try {
+        $data = $source->loadMultiple([]);
+        if (!$data) {
+          return;
+        }
       }
-    }
-    catch (\Exception $e) {
-      \Drupal::logger('helfi_kymp_content')->error(
-        'unable to fetch data while running reindex event'
-      );
-    }
+      catch (\Exception $e) {
+        \Drupal::logger('helfi_kymp_content')->error(
+          'unable to fetch data while running reindex event'
+        );
+      }
 
-    $index->trackItemsInserted($source->getPluginId(), array_keys($data));
+      $index->trackItemsInserted($source->getPluginId(), array_keys($data));
+    }
   }
 
 }
