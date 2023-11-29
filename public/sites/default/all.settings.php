1be03c6fd2cec2f317ba4a286958c5ea7186d024
tuutti <tuutti@iki.fi>
2023-11-29 11:05:48 +0200

    UHF-8943: Preflight checks

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -32,3 +32,21 @@ $config['elastic_proxy.settings']['elastic_proxy_url'] = getenv('ELASTIC_PROXY_U
 
 // Sentry DSN for React.
 $config['react_search.settings']['sentry_dsn_react'] = getenv('SENTRY_DSN_REACT');
+
+$additionalEnvVars = [
+  'AZURE_BLOB_STORAGE_SAS_TOKEN|BLOBSTORAGE_SAS_TOKEN',
+  'AZURE_BLOB_STORAGE_NAME',
+  'AZURE_BLOB_STORAGE_CONTAINER',
+  'DRUPAL_VARNISH_HOST',
+  'DRUPAL_VARNISH_PORT',
+  'PROJECT_NAME',
+  'DRUPAL_API_ACCOUNTS',
+  'DRUPAL_VAULT_ACCOUNTS',
+  'REDIS_HOST',
+  'REDIS_PORT',
+  'REDIS_PASSWORD',
+];
+
+foreach ($additionalEnvVars as $var) {
+  $preflight_checks['environmentVariables'][] = $var;
+}
