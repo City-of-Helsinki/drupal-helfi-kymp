800721d4f72af480bf65371828c26993d3e25a98
tuutti <tuutti@iki.fi>
2023-11-29 13:03:59 +0200

    UHF-8943: Fixes

--- docker/openshift/entrypoints/10-preflight.sh
+++ docker/openshift/entrypoints/10-preflight.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-source /init.sh
-
-if [ ! -f "../docker/openshift/preflight.php" ]; then
-  echo "Preflight not enabled. Skipping ..."
-  exit 0
+if [ -f "../docker/openshift/preflight.php" ]; then
+  source /init.sh
+  echo "Running preflight checks ..."
+  php ../docker/openshift/preflight/preflight.php
 fi
 
-echo "Running preflight checks ..."
-php ../docker/openshift/preflight/preflight.php
