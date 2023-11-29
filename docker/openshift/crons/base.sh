452e80768c19fde93cd29ab7fc9acdaf8f51cfc6
tuutti <tuutti@iki.fi>
2023-11-29 07:29:34 +0200

    UHF-8943: Added init.sh

--- docker/openshift/crons/base.sh
+++ docker/openshift/crons/base.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-# Checking if a new deployment is in progress, as we should not run cron while deploying.
-if [ ! -n "$OPENSHIFT_BUILD_NAME" ]; then
-  echo "OPENSHIFT_BUILD_NAME is not defined. Exiting early."
-  exit 1
-fi
-
-function get_deploy_id {
-  echo $(cat sites/default/files/deploy.lock)
-}
-
-function deployment_in_progress {
-  if [ "$(get_deploy_id)" != "$OPENSHIFT_BUILD_NAME" ]; then
-    return 1
-  fi
-
-  if [ "$(drush state:get system.maintenance_mode)" = "1" ]; then
-    return 1
-  fi
-
-  return 0
-}
+source /init.sh
 
 ATTEMPTS=0
+# Checking if a new deployment is in progress, as we should not run cron while deploying.
 while [ deployment_in_progress ]
 do
   let ATTEMPTS++
 
   if (( ATTEMPTS > 10 )); then
-    echo "Failed to start a new cron pod - deployment probably failed"
+    echo "Failed to start a new cron pod - deployment probably failed."
     exit 1
   fi
+
   echo "A deployment is in progress - waiting for completion ..."
   sleep 60
 done
--- docker/openshift/entrypoints/20-deploy.sh
+++ docker/openshift/entrypoints/20-deploy.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
 
-cd /var/www/html/public
-
-function get_deploy_id {
-  echo $(cat sites/default/files/deploy.id)
-}
-
-function set_deploy_id {
-  echo ${1} > sites/default/files/deploy.id
-}
-
-function output_error_message {
-  echo ${1}
-  php ../docker/openshift/notify.php "${1}" || true
-}
+source /init.sh
 
 function rollback_deployment {
   output_error_message "Deployment failed: ${1}"
   set_deploy_id ${2}
   exit 1
 }
-
-if [ ! -d "sites/default/files" ]; then
-  output_error_message "Container start error: Public file folder does not exist. Exiting early."
-  exit 1
-fi
-
-# Make sure we have active Drupal configuration.
-if [ ! -f "../conf/cmi/system.site.yml" ]; then
-  output_error_message "Container start error: Codebase is not deployed properly. Exiting early."
-  exit 1
-fi
-
-if [ ! -n "$OPENSHIFT_BUILD_NAME" ]; then
-  output_error_message "Container start error: OPENSHIFT_BUILD_NAME is not defined. Exiting early."
-  exit 1
-fi
 
 # Populate twig caches.
 if [ ! -d "/tmp/twig" ]; then
@@ -61,9 +32,8 @@ if [ "$CURRENT_DEPLOY_ID" != "$OPENSHIFT_BUILD_NAME" ]; then
   if [ $? -ne 0 ]; then
     rollback_deployment "Failed to enable maintenance_mode" $CURRENT_DEPLOY_ID
   fi
-  # Run helfi specific pre-deploy tasks. Allow this to fail in case
-  # the environment is not using the 'helfi_api_base' module.
-  # @see https://github.com/City-of-Helsinki/drupal-module-helfi-api-base
+  # Run pre-deploy tasks.
+  # @see https://github.com/City-of-Helsinki/drupal-module-helfi-api-base/blob/main/documentation/deploy-hooks.md
   drush helfi:pre-deploy || true
   # Run maintenance tasks (config import, database updates etc)
   drush deploy
@@ -72,9 +42,8 @@ if [ "$CURRENT_DEPLOY_ID" != "$OPENSHIFT_BUILD_NAME" ]; then
     rollback_deployment "drush deploy failed with {$?} exit code. See logs for more information." $CURRENT_DEPLOY_ID
     exit 1
   fi
-  # Run helfi specific post deploy tasks. Allow this to fail in case
-  # the environment is not using the 'helfi_api_base' module.
-  # @see https://github.com/City-of-Helsinki/drupal-module-helfi-api-base
+  # Run post-deploy tasks.
+  # @see https://github.com/City-of-Helsinki/drupal-module-helfi-api-base/blob/main/documentation/deploy-hooks.md
   drush helfi:post-deploy || true
   # Disable maintenance mode
   drush state:set system.maintenance_mode 0 --input-format=integer
