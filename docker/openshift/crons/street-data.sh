3114096f6f83244717833d7e1a5a6fbef935464e
rpnykanen <48206082+rpnykanen@users.noreply.github.com>
2023-12-18 14:09:46 +0200

    only update street_data once a day, not every index.

--- docker/openshift/crons/street-data.sh
+++ docker/openshift/crons/street-data.sh
@@ -4,6 +4,6 @@ echo "Start indexing street data from external datasource: $(date)"
 
 while true
 do
-  drush sapi-r && drush sapi-i
+  drush sapi-r street_data && drush sapi-i street_data
   sleep 86400
 done
