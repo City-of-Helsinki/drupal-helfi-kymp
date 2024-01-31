801e1f0017a46b5de9470b8e6bcf8aedab306f69
tuutti <tuutti@iki.fi>
2024-01-31 10:35:47 +0200

    UHF-9392: Deployment preflight checks

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -32,3 +32,31 @@ $config['elastic_proxy.settings']['elastic_proxy_url'] = getenv('ELASTIC_PROXY_U
 
 // Sentry DSN for React.
 $config['react_search.settings']['sentry_dsn_react'] = getenv('SENTRY_DSN_REACT');
+
+$additionalEnvVars = [
+  'AZURE_BLOB_STORAGE_SAS_TOKEN|BLOBSTORAGE_SAS_TOKEN',
+  'AZURE_BLOB_STORAGE_NAME',
+  'AZURE_BLOB_STORAGE_CONTAINER',
+  'DRUPAL_VARNISH_HOST',
+  'DRUPAL_VARNISH_PORT',
+  'PROJECT_NAME',
+  'DRUPAL_API_ACCOUNTS',
+  'DRUPAL_VAULT_ACCOUNTS',
+  'REDIS_HOST',
+  'REDIS_PORT',
+  'REDIS_PASSWORD',
+  'TUNNISTAMO_CLIENT_ID',
+  'TUNNISTAMO_CLIENT_SECRET',
+  'TUNNISTAMO_ENVIRONMENT_URL',
+  'SENTRY_DSN',
+  'SENTRY_ENVIRONMENT',
+  // Project specific variables.
+  'ELASTIC_PROXY_URL',
+  'ELASTICSEARCH_URL',
+  'ELASTIC_USER',
+  'ELASTIC_PASSWORD',
+  'SENTRY_DSN_REACT',
+];
+foreach ($additionalEnvVars as $var) {
+  $preflight_checks['environmentVariables'][] = $var;
+}
