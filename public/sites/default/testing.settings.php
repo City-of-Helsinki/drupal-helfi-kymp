8226ee0588639bb74ab1ab4a006af033a507470a
tuutti <tuutti@iki.fi>
2024-02-13 11:09:41 +0200

    UHF-9546: Removed unnecessary settings

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,11 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/kaupunkiymparisto-ja-liikenne/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'liikenne-assets';
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'urban-environment-and-traffic',
-  'fi' => 'kaupunkiymparisto-ja-liikenne',
-  'sv' => 'stadsmiljo-och-trafik',
-  'ru' => 'urban-environment-and-traffic',
-  'zxx' => 'urban-environment-and-traffic',
-];
