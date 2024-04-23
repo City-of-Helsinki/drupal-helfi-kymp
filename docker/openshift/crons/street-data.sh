72fa6c0b7ca10a5f3f090c85c5efa2fc31d75812
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-04-23 12:51:39 +0300

    Reduce API requests by increasing batch size

--- docker/openshift/crons/street-data.sh
+++ docker/openshift/crons/street-data.sh
@@ -4,6 +4,6 @@ echo "Start indexing street data from external datasource: $(date)"
 
 while true
 do
-  drush sapi-r street_data && drush sapi-i street_data
+  drush sapi-r street_data && drush sapi-i street_data --batch-size=500
   sleep 86400
 done
