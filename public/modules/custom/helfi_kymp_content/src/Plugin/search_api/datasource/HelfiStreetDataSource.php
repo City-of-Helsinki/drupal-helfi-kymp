d1ed6098d255b07d28c3329071e57026cd27a485
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-11-28 13:58:31 +0200

    Refactor street data api calls to separate service

--- public/modules/custom/helfi_kymp_content/src/Plugin/search_api/datasource/HelfiStreetDataSource.php
+++ public/modules/custom/helfi_kymp_content/src/Plugin/search_api/datasource/HelfiStreetDataSource.php
@@ -6,13 +6,10 @@ namespace Drupal\helfi_kymp_content\Plugin\search_api\datasource;
 
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\TypedData\ComplexDataInterface;
-use Drupal\Core\TypedData\TypedDataTrait;
 use Drupal\helfi_kymp_content\Plugin\DataType\StreetData;
+use Drupal\helfi_kymp_content\StreetDataService;
 use Drupal\search_api\Datasource\DatasourceInterface;
 use Drupal\search_api\Datasource\DatasourcePluginBase;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\GuzzleException;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -26,31 +23,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class HelfiStreetDataSource extends DatasourcePluginBase implements DatasourceInterface {
 
-  use TypedDataTrait;
-
-  public const API_URL = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs';
-
   /**
    * The client.
-   *
-   * @var \GuzzleHttp\ClientInterface
    */
-  protected ClientInterface $client;
-
-  /**
-   * The logger.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  protected LoggerInterface $logger;
+  protected StreetDataService $client;
 
   /**
    * {@inheritDoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->client = $container->get('http_client');
-    $instance->logger = $container->get('logger.channel.helfi_kymp_content');
+    $instance->client = $container->get(StreetDataService::class);
     return $instance;
   }
 
@@ -73,74 +56,13 @@ class HelfiStreetDataSource extends DatasourcePluginBase implements DatasourceIn
    * {@inheritdoc}
    */
   public function loadMultiple(array $ids): array {
-    $query = http_build_query([
-      'request' => 'GetFeature',
-      'service' => 'WFS',
-      'version' => '1.1.0',
-      'typeName' => 'avoindata:YLRE_Katualue_alue',
-      'propertyname' => 'avoindata:kadun_nimi,avoindata:kayttotarkoitus,avoindata:yllapitoluokka,avoindata:pituus',
-    ]);
-    $uri = sprintf('%s?%s', self::API_URL, $query);
+    $streetData = $this->client->getStreetData();
 
-    try {
-      $content = $this->client->request('GET', $uri);
-      $xmlResult = $content->getBody()->getContents();
-      if (!$xmlResult) {
-        return [];
-      }
-    }
-    catch (GuzzleException $e) {
-      $this->logger->error("Errors while fetching street data from kartta.hel.fi: {$e->getMessage()}");
-      return [];
+    if ($ids) {
+      return array_intersect_key($streetData, array_flip($ids));
     }
 
-    libxml_use_internal_errors(TRUE);
-    $doc = new \DOMDocument(encoding: 'UTF-8');
-    $doc->loadXML($xmlResult);
-    $errors = libxml_get_errors();
-
-    if ($errors) {
-      $this->logger->error('Errors while parsing street data xml string.');
-      return [];
-    }
-
-    $data = [];
-    foreach ($doc->firstChild->firstChild->childNodes->getIterator() as $street_data) {
-      $id = $street_data->getAttribute('gml:id');
-
-      if (!$id || $ids && !in_array($id, $ids)) {
-        continue;
-      }
-
-      $single_street = [
-        'id' => $id,
-      ];
-
-      foreach ($street_data->childNodes->getIterator() as $field) {
-        switch ($field->nodeName) {
-          case 'avoindata:kadun_nimi':
-            $single_street['street_name'] = $field->nodeValue;
-            break;
-
-          case 'avoindata:pituus':
-            $single_street['length'] = $field->nodeValue;
-            break;
-
-          case 'avoindata:yllapitoluokka':
-            // Turn field value from III or II to 3 or 2 etc.
-            $single_street['maintenance_class'] = strlen($field->nodeValue);
-            break;
-        }
-      }
-
-      $street_data_definition = $this->getTypedDataManager()->createDataDefinition('street_data');
-      /** @var \Drupal\Core\TypedData\ComplexDataInterface $street_data */
-      $street_data = $this->getTypedDataManager()->create($street_data_definition);
-      $street_data->setValue($single_street);
-      $data[$id] = $street_data;
-    }
-
-    return $data;
+    return $streetData;
   }
 
   /**
---
+++ public/modules/custom/helfi_kymp_content/src/StreetDataService.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_kymp_content;
+
+use Drupal\Core\TypedData\TypedDataManagerInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * Service for fetching street data from kartta.hel.fi.
+ */
+final readonly class StreetDataService {
+
+  public const API_URL = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs';
+
+  /**
+   * Constructs a new StreetDataService instance.
+   */
+  public function __construct(
+    protected ClientInterface $client,
+    protected TypedDataManagerInterface $typedDataManager,
+    #[Autowire(service: 'logger.channel.helfi_kymp_content')]
+    protected LoggerInterface $logger,
+  ) {
+  }
+
+  /**
+   * Gets street data.
+   *
+   * @return \Drupal\helfi_kymp_content\Plugin\DataType\StreetData[]
+   *   Street data.
+   */
+  public function getStreetData(): array {
+    $query = http_build_query([
+      'request' => 'GetFeature',
+      'service' => 'WFS',
+      'version' => '1.1.0',
+      'typeName' => 'avoindata:YLRE_Katualue_alue',
+      'propertyname' => 'avoindata:kadun_nimi,avoindata:yllapitoluokka,avoindata:pituus',
+    ]);
+    $uri = sprintf('%s?%s', self::API_URL, $query);
+
+    try {
+      $content = $this->client->request('GET', $uri);
+      $xmlResult = $content->getBody()->getContents();
+      if (!$xmlResult) {
+        return [];
+      }
+    }
+    catch (GuzzleException $e) {
+      $this->logger->error("Errors while fetching street data from kartta.hel.fi: {$e->getMessage()}");
+      return [];
+    }
+
+    libxml_use_internal_errors(TRUE);
+    $doc = new \DOMDocument(encoding: 'UTF-8');
+    $doc->loadXML($xmlResult);
+    $errors = libxml_get_errors();
+
+    if ($errors) {
+      $this->logger->error('Errors while parsing street data xml string.');
+      return [];
+    }
+
+    $data = [];
+    foreach ($doc->firstChild->firstChild->childNodes->getIterator() as $street_data) {
+      $id = $street_data->getAttribute('gml:id');
+      if (!$id) {
+        continue;
+      }
+
+      $single_street = [
+        'id' => $id,
+      ];
+
+      foreach ($street_data->childNodes->getIterator() as $field) {
+        switch ($field->nodeName) {
+          case 'avoindata:kadun_nimi':
+            $single_street['street_name'] = $field->nodeValue;
+            break;
+
+          case 'avoindata:pituus':
+            $single_street['length'] = $field->nodeValue;
+            break;
+
+          case 'avoindata:yllapitoluokka':
+            // Turn field value from III or II to 3 or 2 etc.
+            $single_street['maintenance_class'] = strlen($field->nodeValue);
+            break;
+        }
+      }
+
+      $street_data_definition = $this->typedDataManager->createDataDefinition('street_data');
+      /** @var \Drupal\Core\TypedData\ComplexDataInterface $street_data */
+      $street_data = $this->typedDataManager->create($street_data_definition);
+      $street_data->setValue($single_street);
+      $data[$id] = $street_data;
+    }
+
+    return $data;
+  }
+
+}
